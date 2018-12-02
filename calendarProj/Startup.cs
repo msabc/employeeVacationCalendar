e60ba6b2da240355207006071bf748a6eeaf5998
msabc <matija.sabljic1@hotmail.com>
2018-12-02 21:07:45 +0100

    added CORS for default Angular port

--- calendarProj/Startup.cs
+++ calendarProj/Startup.cs
@@ -26,6 +26,10 @@ namespace calendarProj
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecific", p => p.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
